18398685f0a66a497fbb5c93c9bb8298ce08654c
snowbg <snowbg@gmail.com>
2022-05-30 16:55:04 -0300

    Created FOTO field on Atracao model.

--- pontos_turisticos/urls.py
+++ pontos_turisticos/urls.py
@@ -15,6 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+
 from rest_framework import routers
 from core.api.viewsets import PontoTuristicoViewSet
 from atracoes.api.viewsets import AtracaoViewSet
@@ -35,4 +38,7 @@ router.register(r'comentarios', ComentarioViewSet)
 router.register(r'avaliacoes', AvaliacaoViewSet)
 
 
-urlpatterns = [path('', include(router.urls)), path('admin/', admin.site.urls)]
+urlpatterns = [
+    path('', include(router.urls)),
+    path('admin/', admin.site.urls),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
